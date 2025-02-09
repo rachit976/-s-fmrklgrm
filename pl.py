8b135dff034d8fc2c5efeccc9f57f2beacbfa21d
rachit976 <mukeshsoni38@gmail.com>
2025-02-09 20:22:33 +0530

    Add files via upload

---
+++ pl.py
@@ -0,0 +1,12 @@
+# creating different variables
+y ="rachit"
+x ="11"
+print(y)
+print(x)
+codingal ="codingal"
+print(codingal)
+
+# user i
+# input
+ name =input("enter your name:")
+print("\nHello",name, "\welcome to codingal")
